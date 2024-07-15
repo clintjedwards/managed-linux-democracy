29a4e805ba84914d6cd9b17403151fa1bc3825bf
Clint J Edwards <clint.j.edwards@gmail.com>
2024-07-15 15:08:11 -0400

    update thingdoer to display something

--- thingdoer/src/main.rs
+++ thingdoer/src/main.rs
@@ -1,8 +1,37 @@
 //! This simply just does busy work to take up CPU time.
 
 const SCHED_EXT: i32 = 7;
+use colored::Colorize;
+use rand::prelude::SliceRandom;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let word_choice = vec![
+        "is the best ever",
+        "are all flawless programmers",
+        "are 100x engineers",
+        "are 10x engineers",
+        "are the absolute kindest",
+        "are all future presidents",
+        "are truly inspiring",
+        "make the world a better place",
+        "are incredibly talented",
+        "are the heart and soul of the team",
+        "have the best ideas",
+        "are unstoppable",
+        "bring joy to everyone around them",
+        "are the definition of excellence",
+        "make magic happen",
+        "are shining examples of greatness",
+        "are a dream team",
+        "are heroes in disguise",
+        "have a bright future ahead",
+        "are simply the best",
+    ];
+
+    let name = &args[1];
+
     unsafe {
         let pid = libc::getpid();
         let param = libc::sched_param { sched_priority: 0 };
@@ -12,6 +41,9 @@ fn main() {
     }
 
     loop {
-        let _ = 1 + 1;
+        let mut rng = rand::thread_rng();
+        let choice = word_choice.choose(&mut rng).unwrap();
+        println!("{} {}!", name.yellow(), choice);
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
